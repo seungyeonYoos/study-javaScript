1b00fda5454db68e436274fb2f4120c49993589e
ttyyree <cjsrnr90218@gmail.com>
2023-02-16 18:34:14 +0900

    Javascript 공부

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
+
 app.set("view engine", "ejs");
-app.use("/static", express.static(__dirname + "/static"));
+app.use("/static", express.static("static"));
 // app.use(express.static(__dirname + "/veiws"));
 
 const port = 8000;
