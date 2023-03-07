d97d413d05f92d54b3cafee2648e2950efcba122
ttyyree <cjsrnr90218@gmail.com>
2023-03-07 22:45:38 +0900

    javaScript 공부

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 8000;
 // 전역으로 사용할 변수
 
 app.get("/", (req, res) => {
-  res.render("root");
+  res.render("index");
 });
 app.get("/success", (req, res) => {
   res.render("success");
--- static/index.js
+++ static/index.js
@@ -16,13 +16,23 @@
 //     $('.tab-button').eq(2).addClass('orange');
 //     $('.tab-content').eq(2).addClass('show');
 // });
-let aaa = document.getElementsByClassName('tab-button').length;
+// let aaa = document.getElementsByClassName('tab-button').length;
 
-for ( let i =0; i < aaa; i++){
-    $('.tab-button').eq(i).on('click',function(){
+// for ( let i =0; i < aaa; i++){
+//     탭열기(i);
+// }
+
+function 탭열기 (aa){
+    $('.tab-button').eq(aa).on('click',function(){
         $('.tab-button').removeClass('orange');
         $('.tab-content').removeClass('show');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').eq(i).addClass('show');
+        $('.tab-button').eq(aa).addClass('orange');
+        $('.tab-content').eq(aa).addClass('show');
     });
 }
+
+$('.list').on('click',function(e){
+    if(e.target == document.getElementsByClassName('tab-button')[0]){
+        탭열기(aa);
+    }
+});
